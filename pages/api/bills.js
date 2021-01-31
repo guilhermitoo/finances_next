9c697f42e31afb9d697d4c5700ff7088aaf711a4
Guilherme Dias <gh.dias1995@gmail.com>
2021-01-31 09:01:43 -0300

    ajuste cors

--- pages/api/bills.js
+++ pages/api/bills.js
@@ -1,7 +1,20 @@
 import api,{loadDB} from '../../lib/db.js';
+import Cors from 'cors';
+import initMiddleware from '../../lib/init-middleware';
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+      // Only allow requests with GET, POST and OPTIONS
+      methods: ['GET', 'POST', 'OPTIONS'],
+    })
+  )
 
 export default async (req, res) => {
     res.statusCode = 200;
+
+    await cors(req,res);
     
     // let db = loadDB().database();
     // return firebase.database().ref('/bills/').once('value').then((snapshot) => {
